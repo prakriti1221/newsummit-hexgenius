66b5884673754d74c82bd226f9a67a57692cf5b3
saphal-s <subedisaphal0992gmail.com>
2023-07-19 22:31:17 +0545

    add modules

---
+++ backend/models/Booking.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const bookingSchema = new mongoose.Schema(
+  {
+    bookingStatus: {
+      type: String,
+      default: "Not Processed",
+      enum: ["Not Processed", "Processing", "Cancelled", "Completed"],
+    },
+    isParked: {
+      type: String,
+      default: "Parking",
+      enum: ["Parking", "Completed"],
+    },
+    reserveSpace: {
+      type: Number,
+      default: 0,
+    },
+    entryTime: {
+      type: String,
+    },
+    outingTime: {
+      type: String,
+    },
+    amount: {
+      type: Number,
+    },
+    userId: {
+      type: ObjectId,
+      ref: "User",
+    },
+    vehicelId: {
+      type: ObjectId,
+      ref: "Vehicle",
+    },
+    receivedBy: {
+      type: ObjectId,
+      ref: "Parking",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Booking", bookingSchema);
